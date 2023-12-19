b7529d4147cdb4fbc3d47928e9cedd0458260db8
Gen Ichino <zenoplex@gmail.com>
2023-12-20 00:18:19 +0900

    feat: Add screensize and size variables, and implement screen wrapping in _integrate_forces()

--- player/player.gd
+++ player/player.gd
@@ -7,11 +7,15 @@ enum { INIT, ALIVE, INVULNERABLE, DEAD }
 var state: int = INIT
 var thrust: Vector2 = Vector2.ZERO
 var rotation_dir: float = 0.0
+var screensize: Vector2 = Vector2.ZERO
+var size: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	linear_damp = 1.0
 	angular_damp = 5.0
+	screensize = get_viewport_rect().size
+	size = $CollisionShape2D.shape.get_rect().size
 	# TODO: set to INIT after testing
 	change_state(ALIVE)
 
@@ -46,3 +50,11 @@ func get_input() -> void:
 func _physics_process(_delta: float) -> void:
 	constant_force = thrust
 	constant_torque = rotation_dir * spin_power
+
+func _integrate_forces(physics_state: PhysicsDirectBodyState2D) -> void:
+	var half_size = size / 2
+	var transform2d = physics_state.transform
+	transform2d.origin.x = wrap(transform2d.origin.x, -half_size.x , screensize.x + half_size.x)
+	transform2d.origin.y = wrap(transform2d.origin.y, -half_size.y , screensize.y + half_size.y)
+	physics_state.transform = transform2d
+	
