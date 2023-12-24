f07928f1170a575d28a6718558745e493adf7a87
Gen Ichino <zenoplex@gmail.com>
2023-12-25 01:47:17 +0900

    refactor: Infer variable type annotations in main.gd

--- Main/main.gd
+++ Main/main.gd
@@ -1,8 +1,9 @@
 extends Node
 @export var rock_scene: PackedScene
 
+# Type annotation is required for for loops
 const offsets: Array[int] = [-1 ,1]
-var screensize: Vector2 = Vector2.ZERO
+var screensize := Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +12,7 @@ func _ready() -> void:
 	for i in 3:
 		$RockPath/RockSpawn.progress = randi()
 		var pos: Vector2 = $RockPath/RockSpawn.position
-		var vel: Vector2 = Vector2.RIGHT.rotated(randf_range(0, TAU)) * randf_range(50, 125)
+		var vel := Vector2.RIGHT.rotated(randf_range(0, TAU)) * randf_range(50, 125)
 		print(vel)
 		spawn_rock(pos, vel, i + 1)
 
@@ -20,7 +21,7 @@ func _process(delta: float) -> void:
 	pass
 
 func spawn_rock(position: Vector2, velocity: Vector2, size: int) -> void:
-	var node: Node = rock_scene.instantiate()
+	var node := rock_scene.instantiate()
 	node.screensize = screensize
 	node.start(position, velocity, size)
 	call_deferred("add_child", node)
@@ -32,6 +33,6 @@ func _on_rock_exploded(size: int, radius: int, position: Vector2, linear_velocit
 	
 	for offset in offsets:
 		var direction: Vector2 = $Player.position.direction_to(position).orthogonal() * offset
-		var new_position: Vector2 = position + direction * radius
-		var new_velocity: Vector2 = direction * linear_velocity.length() * 1.1
+		var new_position := position + direction * radius
+		var new_velocity := direction * linear_velocity.length() * 1.1
 		spawn_rock(new_position, new_velocity, size - 1)
