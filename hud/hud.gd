16e435ee1dc2cc24a73091bd4b2cc5e63e8d241e
Gen Ichino <zenoplex@gmail.com>
2023-12-25 00:06:32 +0900

    feat: Add new methods and signals to HUD script

--- hud/hud.gd
+++ hud/hud.gd
@@ -16,7 +16,23 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_start_button_pressed() -> void:
 	start_button.hide()
 	start_game.emit()
+
+func _on_timer_timeout() -> void:
+	message_label.hide()
+	message_label.text = ""
+
+func show_message(text: String) -> void:
+	message_label.text = text
+	message_label.show()
+	$Timer.start()
+
+func update_score(score: int) -> void:
+	score_label.text = str(score)
+
+func update_lives(lives: int) -> void:
+	var children: Array[Node] = lives_counter.get_children()
+	for index in len(children):
+		children[index].visible = index > lives
