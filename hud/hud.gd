1e82b1127934a6ff756f6d1c1fd7eeef0b1bb015
Gen Ichino <zenoplex@gmail.com>
2023-12-24 23:54:49 +0900

    refactor: Add HUD script

---
+++ hud/hud.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+@onready var lives_counter: HBoxContainer = $MarginContainer/HBoxContainer/LivesCounter
+@onready var score_label: Label = $MarginContainer/HBoxContainer/ScoreLabel
+@onready var message_label: Label = $VBoxContainer/MessageLabel
+@onready var start_button: TextureButton = $VBoxContainer/StartButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
