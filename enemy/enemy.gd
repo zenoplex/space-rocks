617e15b07ffa7bb727f3fb04a3483c2ff4829e7b
Gen Ichino <zenoplex@gmail.com>
2023-12-30 23:22:09 +0900

    feat: Update enemy initialization

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -3,8 +3,15 @@ extends Area2D
 
 @export var bullet_scene: PackedScene
 
+var speed := 150
+var rotation_speed := 120
+var health := 3
+var followPath := PathFollow2D.new()
+
 @onready var gunCooldownTimer: Timer = get_node("GunCooldownTimer")
 @onready var explostion: Explosion = get_node("Explosion")
+@onready var sprite2D: Sprite2D = get_node("Sprite2D")
+@onready var enemyPaths: EnemyPaths = get_node("EnemyPaths")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,6 +19,10 @@ func _ready() -> void:
 	gunCooldownTimer.wait_time = 1.5
 	gunCooldownTimer.autostart = true
 	explostion.hide()
+	sprite2D.frame = randi() % 3
+	var path := enemyPaths.get_children()[randi() % enemyPaths.get_child_count()]
+	path.add_child(followPath)
+	followPath.loop = false
 
 func _on_gun_cooldown_timer_timeout() -> void:
 	pass # Replace with function body.
