73fbf8505544f791422b4b2ac63e8b1a07c03977
Gen Ichino <zenoplex@gmail.com>
2023-12-31 23:49:55 +0900

    feat: Add area_entered signal handler to bullet.gd

--- bullet/bullet.gd
+++ bullet/bullet.gd
@@ -21,3 +21,9 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("rocks"):
 		body.explode()
 		queue_free()
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is Enemy:
+		var node := area as Enemy
+		node.take_damage(1)		
+ 
