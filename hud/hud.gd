15f48ceb9c58d14f4e5335c184fded034e337a86
Gen Ichino <zenoplex@gmail.com>
2024-01-02 23:36:01 +0900

    refactor: Remove unused functions

--- hud/hud.gd
+++ hud/hud.gd
@@ -16,15 +16,6 @@ const bar_textures: Dictionary = {
 	"red": preload("./bar_red_200.png")
 }
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _on_start_button_pressed() -> void:
 	start_button.hide()
 	start_game.emit()
